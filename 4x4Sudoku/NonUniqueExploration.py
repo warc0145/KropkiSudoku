b2b73a4a0f8af500afa4b2d2d8af2787ad14b45d
warc0145 <dylanmw2003@gmail.com>
2025-05-20 12:11:22 -0600

    Create NonUniqueExploration.py

---
+++ 4x4Sudoku/NonUniqueExploration.py
@@ -0,0 +1,12 @@
+'''
+Name: Dylan Warcholik
+
+File Name: NonUniqueExploration.py
+
+Date Started: 5/20/25
+
+Description:
+This program will explore the 8 Kropki arrangements that resort in non-unique solutions.
+'''
+from KropkiGenerator4x4 import
+from SudokuPuzzleGenerator4x4 import
