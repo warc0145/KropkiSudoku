5cd9ae6f08bfdc6a80aecc1d0e1c64281eefe507
warc0145 <dylanmw2003@gmail.com>
2025-05-20 12:34:21 -0600

    Update NonUniqueExploration.py's import statements

--- 4x4Sudoku/NonUniqueExploration.py
+++ 4x4Sudoku/NonUniqueExploration.py
@@ -8,5 +8,10 @@ Date Started: 5/20/25
 Description:
 This program will explore the 8 Kropki arrangements that resort in non-unique solutions.
 '''
-from KropkiGenerator4x4 import
-from SudokuPuzzleGenerator4x4 import
+from NonUniqueKropkiGenerator import find_horizontal_kropki_dots, find_vertical_kropki_dots
+from SudokuPuzzleGenerator4x4 import solutions
+
+for i in range(10):
+  print(i)
+  print(find_horizontal_kropki_dots(solutions[i]))
+  print(find_vertical_kropki_dots(solutions[i]))
