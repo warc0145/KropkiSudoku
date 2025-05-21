d18667217be4c68b1ec3c89a19dac4744b404729
warc0145 <dylanmw2003@gmail.com>
2025-05-21 16:50:40 -0600

    Update NonUniqueExploration.py to print out boards with the arrangement that it goes with

--- 4x4Sudoku/NonUniqueExploration.py
+++ 4x4Sudoku/NonUniqueExploration.py
@@ -11,6 +11,7 @@ This program will explore the 8 Kropki arrangements that resort in non-unique so
 from NonUniqueKropkiGenerator import kropki_solutions
 from SudokuPuzzleGenerator4x4 import solutions, print_board
 
+duplicate_arrangements = []
 for i in range(len(kropki_solutions)):
     num_dots = 0 # Will be used to count the number of dots in the repeating solutions
     for direction in kropki_solutions[i]: # Will go through the horizontal dots then the vertical dots
@@ -20,6 +21,14 @@ for i in range(len(kropki_solutions)):
                   num_dots += 1
     count = kropki_solutions.count(kropki_solutions[i])
     if count > 1:
+        duplicate_arrangements.append(kropki_solution[i])
         print("The Kropki Arrangement", kropki_solutions[i], "appears", count, "times. It has", num_dots,"Kropki dots. Here is a board with that arrangement:")
         print_board(solutions[i])
-        
+
+# Consider all duplicate_arrangements, and print out the two boards that align with those arrangements
+for sol in duplicate_arrangements:
+    print("For the arrangement", sol, "Here are the two solutions:")
+    for i in range(len(kropki_solutions)):
+        if kropki_solutions[i] == sol:
+            print_board(solutions[i])
+    
