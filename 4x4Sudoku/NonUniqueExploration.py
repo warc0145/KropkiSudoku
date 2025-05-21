ce1e1f08a7573bc0cfa932ab692044a55dbd9623
warc0145 <dylanmw2003@gmail.com>
2025-05-21 10:36:54 -0600

    Update NonUniqueExploration.py to print out boards with non-unique arrangements and count the dots on those arrangements..

--- 4x4Sudoku/NonUniqueExploration.py
+++ 4x4Sudoku/NonUniqueExploration.py
@@ -8,10 +8,18 @@ Date Started: 5/20/25
 Description:
 This program will explore the 8 Kropki arrangements that resort in non-unique solutions.
 '''
-from NonUniqueKropkiGenerator import find_horizontal_kropki_dots, find_vertical_kropki_dots
-from SudokuPuzzleGenerator4x4 import solutions
+from NonUniqueKropkiGenerator import kropki_solutions
+from SudokuPuzzleGenerator4x4 import solutions, print_board
 
-for i in range(10):
-  print(i)
-  print(find_horizontal_kropki_dots(solutions[i]))
-  print(find_vertical_kropki_dots(solutions[i]))
+for i in range(len(kropki_solutions)):
+    num_dots = 0 # Will be used to count the number of dots in the repeating solutions
+    for direction in kropki_solutions[i]: # Will go through the horizontal dots then the vertical dots
+        for row in direction: 
+            for column in row:
+                if not (column == 0):
+                  num_dots += 1
+    count = kropki_solutions.count(kropki_solutions[i])
+    if count > 1:
+        print("The Kropki Arrangement", kropki_solutions[i], "appears", count, "times. It has", num_dots,"Kropki dots. Here is a board with that arrangement:")
+        print_board(solutions[i])
+        
