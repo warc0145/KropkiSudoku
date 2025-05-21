05145e8f22d716e66c008251d04d18a3c283adfe
warc0145 <dylanmw2003@gmail.com>
2025-05-21 15:12:10 -0600

    Update NonUniqueExploration.py to include print_board method

--- 4x4Sudoku/NonUniqueExploration.py
+++ 4x4Sudoku/NonUniqueExploration.py
@@ -11,6 +11,18 @@ This program will explore the 8 Kropki arrangements that resort in non-unique so
 from NonUniqueKropkiGenerator import kropki_solutions
 from SudokuPuzzleGenerator4x4 import solutions, print_board
 
+
+def print_board(board):
+    """
+    Print a visual of the board, which should be a two dimensional, 4x4 array
+    """
+    for row_num in range(4):
+        row = board[row_num]
+        if row_num % 2 == 0:
+            print("-------------")
+        print("|", row[0], row[1], "|", row[2], row[3], "|")
+    print("-------------")
+
 for i in range(len(kropki_solutions)):
     num_dots = 0 # Will be used to count the number of dots in the repeating solutions
     for direction in kropki_solutions[i]: # Will go through the horizontal dots then the vertical dots
