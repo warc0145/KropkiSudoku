ce1ae4cafeffd68113680b75a0f2fb38dfdeb71c
warc0145 <dylanmw2003@gmail.com>
2025-07-13 22:34:43 +0000

    Fixed indexing error

--- 4x4Sudoku/ReverseAndSolve.py
+++ 4x4Sudoku/ReverseAndSolve.py
@@ -12,12 +12,14 @@ and observe if the puzzle is still able to be solved (uniquely).
 from AnyDiamond import diamond_list, no_diamond_list, full_cell_list, no_full_cells_list
 
 def color_reverser(board):
-    for row in range(len(board)):
-        for col in range(len(row)):
-            if abs(board[row][col]) == 1:
-                board[row][col] *= -1 
-                # This is to say, for every dot, reverse the color
+    for dot_type in range(len(board)):
+        # Allows iterating through horizontal and vertical dots
+        for row in range(len(board[dot_type])):
+            for col in range(len(board[dot_type][row])):
+                if abs(board[dot_type][row][col]) == 1:
+                    board[dot_type][row][col] *= -1 
+                    # This is to say, for every dot, reverse the color
 
-print(any_diamond[0])
-color_reverser(any_diamond[0])
-print(any_diamond[0])
+# print(diamond_list[0])
+# color_reverser(diamond_list[0])
+# print(diamond_list[0])
