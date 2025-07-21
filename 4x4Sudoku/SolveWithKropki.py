6a9d511667d8487ba6f95c05df1da405e9cd1c13
warc0145 <dylanmw2003@gmail.com>
2025-07-21 15:23:05 +0000

    SolveWithKropki.py can now find multiple solutions where they exist

--- 4x4Sudoku/SolveWithKropki.py
+++ 4x4Sudoku/SolveWithKropki.py
@@ -20,78 +20,91 @@ def check_kropki(current_board, current_row, current_column, kropki_sol, guess):
 
     Will only check dots above and to the left, since cells to the right and below will not be filled yet.
     """
+    # In the case that we are in position (0,0), there are no dots to check, so these are the default values:
+    vertical_valid = True
+    horizontal_valid = True
     # print("Current board:")
     # print_board(current_board)
     # print("kropki_sol:", kropki_sol)
     # print("row, col, guess:", current_row, current_column, guess, "\n\n")
     # Check horizontal dot (to the left)
+    
+
     if current_column > 0:
         k_col = current_column - 1 # Location of the dot to the left of the current position
         if kropki_sol[0][current_row][k_col] == -1:
             difference = guess - current_board[current_row][k_col]
             if difference == 1 or difference == -1:
-                return True
+                horizontal_valid = True
             else:
-                return False
+                horizontal_valid = False
 
         elif kropki_sol[0][current_row][k_col] == 1:
             quotient = guess / current_board[current_row][k_col]
             if quotient == 2 or quotient == 0.5:
-                return True
+                horizontal_valid = True
             else:
-                return False
+                horizontal_valid = False
 
         else: # 0, so no dot, so the cells must NOT have a difference of one or quotient of 2
             difference = guess - current_board[current_row][k_col]
             if difference == 1 or difference == -1:
-                return False
+                horizontal_valid = False
             else:
                 # print(current_board)
                 # print(solutions)
                 quotient = guess / current_board[current_row][k_col]
                 if quotient == 2 or quotient == 0.5:
-                    return False
+                    horizontal_valid = False
                 else:
-                    return True
+                    horizontal_valid = True
     
     # Check vertical dots
     if current_row > 0:
         k_row = current_row - 1 # Location of the dot to the left of the current position
         if kropki_sol[1][k_row][current_column] == -1:
+            # print("We are guessing a value for the location", current_row, current_column, "which has a white dot to the left")
+            # print("Our guess is", guess, "and the value to the left is")
             difference = guess - current_board[k_row][current_column]
             if difference == 1 or difference == -1:
-                return True
+                vertical_valid = True
             else:
-                return False
+                vertical_valid = False
 
         elif kropki_sol[1][k_row][current_column] == 1:
             quotient = guess / current_board[k_row][current_column]
             if quotient == 2 or quotient == 0.5:
-                return True
+                vertical_valid = True
+
             else:
-                return False
+                vertical_valid = False
 
         else: # 0, so no dot, so the cells must NOT have a difference of one or quotient of 2
             difference = guess - current_board[k_row][current_column]
             if difference == 1 or difference == -1:
-                return False
+                vertical_valid = False
             else:
                 quotient = guess / current_board[k_row][current_column]
                 if quotient == 2 or quotient == 0.5:
-                    return False
+                    vertical_valid = False
                 else:
-                    return True
-    return True # In the case that we are in position (0,0), there are no dots to check
+                    vertical_valid = True
+    if horizontal_valid and vertical_valid:
+        return True
+    else:
+        return False
 
 
 solutions = []
 
-def kropki_solver(current_board, kropki_sol, current_row, current_column):
+def kropki_solver(current_board, kropki_sol, current_row, current_column, solutions):
     """
     A Depth First Search to solve a 4x4 sudoku board according to a given arrangement of Kropki dots.
     """
     guess = 1
     while guess < 5:
+        # print("Current row, column, guess:", current_row, current_column, guess)
+        # print("Current board:", current_board, "\n")
         row_valid = check_row(current_board, current_row, current_column, guess)
 
         if row_valid: # Then we should check the column
@@ -101,32 +114,38 @@ def kropki_solver(current_board, kropki_sol, current_row, current_column):
                 box_valid = check_box(current_board, current_row, current_column, guess)
 
                 if box_valid: # Then we should check the kropki_arrangement
+                    # print("Just called kropki valid with currentboard=", current_board, "and row,col,guess=", current_row, current_column, guess)
                     kropki_valid = check_kropki(current_board, current_row, current_column, kropki_sol, guess)
-
+                    
                     if kropki_valid: # Then our guess is valid!
                         current_board[current_row][current_column] = guess
 
                         if current_column < 3: # move to next column
-                    
-                            return kropki_solver(current_board, kropki_sol, current_row, current_column+1)
-                        
+                            kropki_solver(current_board, kropki_sol, current_row, current_column+1, solutions)
+                            # if type(end_val) == list:
+                            #     return end_val
                         else: # At the end of the current row
                             if current_row < 3: # Move to next row
-                                return kropki_solver(current_board, kropki_sol, current_row + 1, 0)
-                        
+                                kropki_solver(current_board, kropki_sol, current_row + 1, 0, solutions)
+                                # if type(end_val) == list:
+                                #     return end_val
                             else: # We are at the end of the board!                            
                                 solutions.append([row.copy() for row in current_board])
 
                     # After recursively exploring all substates of this valid partial solution, backtrack to continue finding all valid solutions!
                     current_board[current_row][current_column] = 0
+                    
         
         # We arrive here if one of the checks failed OR we've backtracked from all solutions at this point; increment guess and check new answer 
         # if we haven't reached the end of the board with the last guess
-        if guess == 4 and current_row == 3 and current_column == 3:
-            return solutions
-        else:
-            guess += 1
-        
+        guess += 1
+    if current_row == 0 and current_column == 0:
+        return solutions
+
 blank_board = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-print(kropki_solver(blank_board, kropki_solutions[0], 0, 0))
-print(solutions)
+print(kropki_solver(blank_board, kropki_solutions[0], 0, 0, []))
+
+blank_board = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+kropki_solution_eg = ([[-1, -1, -1], [-1, -1, -1], [-1, 0, -1], [-1, 0, -1]], [[0, -1, -1, 0], [-1, -1, -1, -1], [-1, 0, 0, -1]])
+print("Running to check for multiple solutions")
+print(kropki_solver(blank_board, kropki_solution_eg, 0, 0, []))
