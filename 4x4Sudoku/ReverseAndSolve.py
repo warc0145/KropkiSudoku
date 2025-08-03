2c92ed3a187b06eee59966afb25740baeededea9
warc0145 <dylanmw2003@gmail.com>
2025-08-03 05:04:31 +0000

    Added code to reverse ALL puzzles not just those with diamonds

--- 4x4Sudoku/ReverseAndSolve.py
+++ 4x4Sudoku/ReverseAndSolve.py
@@ -12,6 +12,8 @@ and observe if the puzzle is still able to be solved (uniquely).
 from AnyDiamond import diamond_list, no_diamond_list, full_cell_list, no_full_cells_list
 from SolveWithKropki import kropki_solver
 from AltLatexPrinter import latex_print
+from SudokuPuzzleGenerator4x4 import solutions
+from KropkiGenerator4x4 import kropki_solutions
 
 def color_reverser(kropki):
     """
@@ -45,12 +47,28 @@ for i in range(len(diamond_list)):
     if len(all_solutions) > 0:
         fun_solutions.append((k_sol, k_copy))
 
-print("There are", len(fun_solutions), "fun solutions (puzzles with diamonds that are reversible... so 48 )")
-# for sol in fun_solutions:
-#     latex_print(sol[0])
-#     latex_print(sol[1])
-#     print("\\paragraph{\\\\}")
+print("There are", len(fun_solutions), "fun solutions (puzzles with diamonds that are reversible... so 12 and 12)")
+for sol in fun_solutions:
+    latex_print(sol[0])
+    latex_print(sol[1])
+    print("\\paragraph{\\\\}")
 
 # for sol in fun_solutions:
 #     num_sols = len(kropki_solver([[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]], sol[1], 0, 0, []))
 #     print(num_sols)
+
+# Now let's see how many of the 288 puzzles are solvable when colors are reversed:
+# reversible_boards = []
+# for i in range(len(solutions)):
+#     print("Checking board #", i)
+#     k_sol = kropki_solutions[i]
+#     k_copy = color_reverser(k_sol)
+#     blank_board = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+
+#     all_solutions = kropki_solver(blank_board, k_copy, 0, 0, [])
+#     # print(i, "has", len(all_solutions), "solutions")
+#     if len(all_solutions) > 0:
+    
+#         reversible_boards.append((k_sol, k_copy))
+#         print("board #", i, "has", len(all_solutions), "solutions. Appending... Now there are", len(reversible_boards))
+# print("There are", len(reversible_boards), "reversible boards")
