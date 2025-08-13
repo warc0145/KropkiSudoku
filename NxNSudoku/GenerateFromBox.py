86b2ca77b5b19b3685a00ad687f9114653aa9ab5
warc0145 <dylanmw2003@gmail.com>
2025-08-13 17:05:48 -0600

    Create GenerateFromBox.py

---
+++ NxNSudoku/GenerateFromBox.py
@@ -0,0 +1,22 @@
+'''
+Name: Dylan Warcholik
+Date: 8/13/25
+Description:
+As I begin to explore counting unique Kropki arrangements on larger size puzzles, this file will include methods will enumerate the 4x4, 6x6, and 9x9 possible "boxes" and determine the uniqueness
+of the Kropki arrangements of the boxes for all sizes.
+'''
+
+def box_generator_4x4():
+    """
+    Generates the kropki arrangements of a single box from the 4x4 grid, report uniqueness
+    """
+
+def box_generator_6x6():
+    """
+    Generates the kropki arrangements of a single box from the 4x4 grid, report uniqueness
+    """
+
+def box_generator_9x9():
+    """
+    Generates the kropki arrangements of a single box from the 4x4 grid, report uniqueness
+    """
